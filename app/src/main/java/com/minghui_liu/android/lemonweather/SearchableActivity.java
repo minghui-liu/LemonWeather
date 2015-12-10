0f7f1d2939a94cc0d50874bd08686c4d217fdea4
kevin <minghui.liu@trincoll.edu>
2015-12-10 11:32:25 -0500

    Add search configuration file, launch SearchableActivity when searching from search view log search query modify manifest to specify default searchable

--- app/src/main/java/com/minghui_liu/android/lemonweather/SearchableActivity.java
+++ app/src/main/java/com/minghui_liu/android/lemonweather/SearchableActivity.java
@@ -1,23 +1,29 @@
 package com.minghui_liu.android.lemonweather;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by kevin on 11/19/15.
  */
-public class SearchableActivity extends Activity {
+public class SearchableActivity extends ListActivity {
+    private static final String TAG = "LemonWeather";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             //searchCityDatabase(query);
+            Log.d(TAG, "query: " + query);
         }
     }
+
 }
