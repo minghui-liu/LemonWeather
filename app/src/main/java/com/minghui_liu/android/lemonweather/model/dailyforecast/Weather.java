6ddfab7925eae27351012dc7529a9cd2dc6b4b30
kevin <minghui.liu@trincoll.edu>
2015-12-20 18:06:18 -0500

    Add landscape view of weather fragment landscape view displays wind, cloud, pressure, humidity add hourly forecast model and api calls add wind, pressure, cloud and humidity icons

--- app/src/main/java/com/minghui_liu/android/lemonweather/model/dailyforecast/Weather.java
+++ app/src/main/java/com/minghui_liu/android/lemonweather/model/dailyforecast/Weather.java
@@ -1,5 +1,5 @@
 
-package com.minghui_liu.android.lemonweather.model.forcast;
+package com.minghui_liu.android.lemonweather.model.dailyforecast;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
